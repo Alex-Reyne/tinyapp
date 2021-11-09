5c6361756bc0bd9d825e931044fff3068e4904dc
Alex-Reyne <creativereyne@gmail.com>
2021-11-08 18:00:54 -0600

    add urls_index view

--- express_server.js
+++ express_server.js
@@ -9,7 +9,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/', (req, res) => {
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.get('/', (req, res) => { 
   res.send('Hello!');
 });
 
