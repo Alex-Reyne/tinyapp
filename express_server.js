1305b4768d9bef39ecb182cf71b24b76b1592c60
Alex-Reyne <creativereyne@gmail.com>
2021-11-09 21:48:18 -0600

    Fix form so it aligns properly to center on url page.

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
-var cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 app.use(cookieParser());
 const PORT = 8080;
 
